77ae4f7f744733333a77718d187ef3404da2e80b
Estefania Sanchez Sanchez <faniaeste92@gmail.com>
2026-02-16 21:12:42 +0100

    Agrego registro y Agrego css

--- appWeb/routes.py
+++ appWeb/routes.py
@@ -6,7 +6,7 @@ from flask import render_template
 
 @app.route("/")
 def base():
-    return render_template("base.html")
+    return render_template("index.html")
 
 @app.route("/ejercicio")
 def ejercicio():
@@ -15,3 +15,7 @@ def ejercicio():
 @app.route("/nutricion")
 def nutricion():
     return render_template("nutricion.html")
+
+@app.route("/data")
+def data():
+    return render_template("data.html")
